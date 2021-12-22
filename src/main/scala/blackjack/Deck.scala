8e11a8405f889f568b11974d1cbdc1a7accdd02f
Roy Straub <roy.straub@codesquad.nl>
2021-12-22 19:26:08 +0100

    More concise

--- src/main/scala/blackjack/Deck.scala
+++ src/main/scala/blackjack/Deck.scala
@@ -3,10 +3,9 @@ package blackjack
 case class Deck(cards: List[Card])
 
 object Deck {
-  val cards: List[Card] = Suits.values.toList.foldRight(List[Card]())((s, l) => {
-    val suitCards = Ranks.values.toList.map(Card(_, s))
-    l.appendedAll(suitCards)
-  })
+  val cards: List[Card] = Suits.values.toList.flatMap(s =>
+    Ranks.values.toList.map(Card(_, s))
+  )
 
   def shuffled(): Deck = Deck(List())
 }
--- src/main/scala/blackjack/Suits.scala
+++ src/main/scala/blackjack/Suits.scala
@@ -3,8 +3,5 @@ package blackjack
 object Suits extends Enumeration {
   type Suit = Value
 
-  val HEARTS: Suits.Value = Value(4)
-  val DIAMONDS: Suits.Value = Value(3)
-  val SPADES: Suits.Value = Value(2)
-  val CLUBS: Suits.Value = Value(1)
+  val HEARTS, DIAMONDS, SPADES, CLUBS = Value
 }
